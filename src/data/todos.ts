3392ef8e45323a1f2080af82e91ed9ee34f640e7
Roman Danyliuk <romandanuliyk@gmail.com>
2025-11-10 16:13:38 +0200

    removed mock data

--- src/data/todos.ts
+++
@@ -1,8 +0,0 @@
-import type { Todo } from "@/types/Todo";
-
-export const todos:Todo[] = [
-  { id: 1, completed: true, title: 'HTML + CSS' },
-  { id: 2, completed: false, title: 'JS' },
-  { id: 3, completed: false, title: 'React' },
-  { id: 4, completed: false, title: 'Vue.js' },
-];
